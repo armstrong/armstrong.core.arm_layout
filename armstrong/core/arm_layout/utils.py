34513dd1716f7299b2244d488055e82da1aea080
Jon Cotton <jcotton@baycitizen.org>
2012-03-05 16:13:18 -0800

    Use a GenericBackend for our template finding function.

--- armstrong/core/arm_layout/utils.py
+++ armstrong/core/arm_layout/utils.py
@@ -1,6 +1,13 @@
 from django.utils.safestring import mark_safe
 from django.template.loader import render_to_string
 
+from armstrong.utils.backends import GenericBackend
+
+
+template_finder = GenericBackend('ARMSTRONG_LAYOUT_TEMPLATE_FINDER',
+        defaults='armstrong.core.arm_layout.utils.get_layout_template_name')\
+        .get_backend
+
 
 def get_layout_template_name(model, name):
     ret = []
@@ -15,5 +22,5 @@ def get_layout_template_name(model, name):
 def render_model(object, name, dictionary=None, context_instance=None):
     dictionary = dictionary or {}
     dictionary["object"] = object
-    return mark_safe(render_to_string(get_layout_template_name(object, name),
+    return mark_safe(render_to_string(template_finder(object, name),
         dictionary=dictionary, context_instance=context_instance))
