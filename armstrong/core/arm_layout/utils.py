2320caffe05c5b5f186f348dc08c538fb5962bd9
Travis Swicegood <development@domain51.com>
2012-03-20 11:22:21 -0500

    Change utils.render_model to be a backend

--- armstrong/core/arm_layout/utils.py
+++ armstrong/core/arm_layout/utils.py
@@ -1,21 +1,12 @@
+# Here for backwards compatibility (deprecated)
 from django.utils.safestring import mark_safe
 from django.template.loader import render_to_string
 
 from armstrong.utils.backends import GenericBackend
 
+render_model = (GenericBackend("ARMSTRONG_RENDER_MODEL_BACKEND",
+        defaults="armstrong.core.arm_layout.backends.BasicRenderModelBackend")
+    .get_backend())
 
-def get_layout_template_name(model, name):
-    ret = []
-    for a in model.__class__.mro():
-        if not hasattr(a, "_meta"):
-            continue
-        ret.append("layout/%s/%s/%s.html" % (a._meta.app_label,
-            a._meta.object_name.lower(), name))
-    return ret
-
-
-def render_model(object, name, dictionary=None, context_instance=None):
-    dictionary = dictionary or {}
-    dictionary["object"] = object
-    return mark_safe(render_to_string(get_layout_template_name(object, name),
-        dictionary=dictionary, context_instance=context_instance))
+# Here for backwards compatibility (deprecated)
+get_layout_template_name = render_model.get_layout_template_name
