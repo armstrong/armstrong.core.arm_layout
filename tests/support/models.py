e97791fdf48921f9133864d5ef290cea722af50e
Jon Cotton <jc@finaltransfer.net>
2014-08-23 22:13:55 -0700

    tests Django 1.7 - remove app loading warning
    "RemovedInDjango19Warning: Model class tests.support.models.Foobar doesn't declare an explicit app_label and either isn't in an application in INSTALLED_APPS or else was imported before its application was loaded."

--- tests/support/models.py
+++ tests/support/models.py
@@ -5,45 +5,68 @@ from armstrong.core.arm_layout.mixins import *
 class Foobar(models.Model):
     title = models.CharField(max_length=100)
 
+    class Meta:
+        app_label = "support"
+
 
 class SubFoobar(Foobar):
-    pass
+    class Meta:
+        app_label = "support"
 
 
 class AbstractFoo(Foobar):
     class Meta:
         abstract = True
+        app_label = "support"
 
 
 class ConcreteFoo(AbstractFoo):
-    pass
+    class Meta:
+        app_label = "support"
 
 
 class ProxyFoo(Foobar):
     class Meta:
         proxy = True
+        app_label = "support"
 
 
 class HasOwnLayoutMethod(models.Model):
     def get_layout_template_name(self, name):
         return ["my_layouts/%s/%s.file" % (type(self).__name__.lower(), name)]
 
+    class Meta:
+        app_label = "support"
 
-class Base(models.Model):
-    pass  # so we can test inheritance
+
+class Base(models.Model):  # so we can test inheritance
+    class Meta:
+        app_label = "support"
 
 
 class TemplatesBySlugModel(TemplatesBySlugMixin, Base):
     slug = models.SlugField()
 
+    class Meta:
+        app_label = "support"
+
 
 class TemplatesByFullSlugModel(TemplatesByFullSlugMixin, Base):
     full_slug = models.CharField(max_length=255, blank=True)
+
+    class Meta:
+        app_label = "support"
 
 
 class TypeWithSlugModel(Base):
     slug = models.SlugField()
 
+    class Meta:
+        app_label = "support"
+
 
 class TemplatesByTypeModel(TemplatesByTypeMixin, Base):
     type = models.ForeignKey(TypeWithSlugModel)
+
+    class Meta:
+        app_label = "support"
